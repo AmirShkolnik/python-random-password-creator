7d94dfb7a50089c6687bdc1234d802ea4c60bda5
ASHKO <amirshkolnik1@gmail.com>
2025-02-04 09:20:03 +0100

    add password list for loop for letters

--- run.py
+++ run.py
@@ -8,3 +8,10 @@ symbols = list("!#$%&()*+")
 nr_letters = random.randint(1,5)
 nr_numbers = random.randint(1,5)
 nr_symbols = random.randint(1,5)
+
+password_list = []
+
+for letter in range(0, nr_letters):
+    password_list += random.choice(letters)
+
+

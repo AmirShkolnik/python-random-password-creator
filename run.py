706b3103ebabaf96cf39ad0f352a8b488f68fb95
ASHKO <amirshkolnik1@gmail.com>
2025-02-04 09:19:22 +0100

    add numbers letters and symbols

--- run.py
+++ run.py
@@ -5,4 +5,6 @@ letters = list(string.ascii_letters)
 numbers = list(string.digits)
 symbols = list("!#$%&()*+")
 
-
+nr_letters = random.randint(1,5)
+nr_numbers = random.randint(1,5)
+nr_symbols = random.randint(1,5)

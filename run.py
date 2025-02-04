b51738805278545879041dae53ee18607bf8c864
ASHKO <amirshkolnik1@gmail.com>
2025-02-04 09:05:05 +0100

    create new folder random password creator

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import string
+
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+symbols = list("!#$%&()*+")
